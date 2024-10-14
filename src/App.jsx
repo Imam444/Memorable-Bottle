367d47573b14e7e75549efacc130290f9a4ebdbf
Imam444 <mdimamhossain565528@gmail.com>
2024-10-14 14:26:45 +0600

    memorable water bottle

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import  Header from './components/Header/Header';
+import Bottles from'./components/Bottles/Bottles';
+import './App.css'
+
+
+function App() {
+  
+
+  return (
+    <>
+     
+      <h1>Vite + React</h1>
+
+      <Header></Header>
+      <Bottles></Bottles>
+      
+      
+    </>
+  )
+}
+
+export default App
